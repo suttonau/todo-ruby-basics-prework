1873dd50053fe3970bc9a924522bfd90306c4c56
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-08-31 00:06:32 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -15,13 +15,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(type = "cheese")
+  return pizza_party
 end
